d5b4f03b9efc1a29b29e6710cc796132ea13a763
b00nw33 <pss.mrteo@gmail.com>
2021-12-21 21:09:21 +0800

    First commit

--- script.js
+++ script.js
@@ -1,4 +1,199 @@
-var main = function (input) {
-  var myOutputValue = 'hello world';
-  return myOutputValue;
-};
+import Deck, { Card } from "./deck.js";
+
+// HTML Handlers
+const dealerCardSlot = document.querySelector(".computer-card-slot");
+const playerCardSlot = document.querySelector(".player-card-slot");
+const deckElement = document.querySelector(".deck");
+const text = document.querySelector(".text");
+
+// button Handlers
+var btnStart = document.getElementById("blackjack-start-button");
+var btnHit = document.getElementById("blackjack-hit-button");
+var btnStand = document.getElementById("blackjack-stand-button");
+
+btnHit.disabled = true;
+btnStand.disabled = true;
+
+let deck,
+  dealerHand = [],
+  playerHand = [];
+
+document.querySelectorAll(".edittable").forEach(function (node) {
+  node.ondblclick = function () {
+    var val = this.textContent;
+    var input = document.createElement("input");
+    input.value = val;
+    input.onblur = function () {
+      var val = this.value;
+      this.parentNode.textContent = val;
+    };
+    this.textContent = "";
+    this.appendChild(input);
+    input.focus();
+  };
+});
+
+function displayCards(cardSlot) {
+  var cards = document.getElementsByClassName("card"),
+    cw = cardSlot.clientWidth,
+    sw = cardSlot.scrollWidth,
+    diff = sw - cw,
+    offset = diff / (cards.length - 1);
+
+  for (var i = 1; i < cards.length; i++) {
+    cards[i].style.transform = "translateX(-" + offset * i + "px)";
+  }
+}
+
+function clgHands() {
+  console.clear();
+  console.table(dealerHand);
+  console.table(playerHand);
+  console.log(
+    `D:${points(dealerHand)} P:${points(playerHand)}
+N:${deck.cards[0].value} ${deck.cards[1].value} ${deck.cards[2].value}`
+  );
+}
+
+function points(handArray) {
+  var totalHandValue = 0;
+  var aceCounter = 0;
+
+  // loop through player or dealer hand and add up the values
+  var index = 0;
+  while (index < handArray.length) {
+    var currentCard = handArray[index];
+
+    totalHandValue += +currentCard.value
+      ? +currentCard.value
+      : currentCard.value == "A"
+      ? 1
+      : 10;
+
+    if (currentCard.value == "A") {
+      totalHandValue += 10;
+      aceCounter += 1;
+    }
+
+    index += 1;
+  }
+
+  index = 0;
+  while (index < aceCounter) {
+    if (totalHandValue > 21) {
+      totalHandValue = totalHandValue - 10;
+    }
+    index += 1;
+  }
+
+  return totalHandValue;
+}
+
+function dealerFinish(addCards, message) {
+  var flippedCard = document.querySelector(".flip");
+
+  // dealer reveal flipped card
+  flippedCard.classList.remove("flip");
+  flippedCard.classList.remove("deck");
+  flippedCard.classList.add("card");
+
+  // dealer bj
+  if (points(dealerHand) === 21) {
+    text.textContent = "Dealer blackjack!";
+  }
+
+  // dealer hits till 17
+  while (points(dealerHand) < 17 && addCards == true) {
+    dealerHand.push(deck.pop());
+    dealerCardSlot.appendChild(dealerHand[dealerHand.length - 1].getHTML());
+  }
+
+  // update deck count
+  deckElement.textContent = deck.numberOfCards;
+
+  // return message
+
+  enableBtnStart(message);
+}
+
+function enableBtnStart(message) {
+  // enable start button
+  btnStart.disabled = false;
+  btnHit.disabled = true;
+  btnStand.disabled = true;
+
+  text.textContent = message;
+}
+
+btnStart.addEventListener("click", function () {
+  deck = new Deck();
+  deck.shuffle();
+
+  // clean before round
+  playerHand = [];
+  dealerHand = [];
+  dealerCardSlot.textContent = "";
+  playerCardSlot.textContent = "";
+  text.textContent = "Hit/Stand";
+  console.clear();
+
+  let bj = [new Card("s", "A"), new Card("S", "K"), new Card("S", "Q")];
+  playerHand.push(bj[0]);
+  playerHand.push(bj[0]);
+  // dealerHand.push(bj[0]);
+  // playerHand.push(bj[1]);
+  // dealerHand.push(bj[2]);
+
+  // initial deal
+  // playerHand.push(deck.pop());
+  dealerHand.push(deck.pop());
+  playerHand.push(deck.pop());
+  dealerHand.push(deck.pop());
+
+  // update html
+  playerHand.forEach((card) => playerCardSlot.appendChild(card.getHTML()));
+  dealerCardSlot.appendChild(dealerHand[0].getHTML());
+  dealerCardSlot.appendChild(dealerHand[1].getHTMLflipped());
+
+  // update deck count
+  deckElement.textContent = deck.numberOfCards;
+
+  // if player bj, check if dealer bj
+  if (points(playerHand) == 21) {
+    if (points(dealerHand) == 21) dealerFinish(false, "Blackjack push!");
+    else dealerFinish(false, "You won with Blackjack!");
+  } else {
+    btnStart.disabled = true;
+    btnHit.disabled = false;
+    btnStand.disabled = false;
+  }
+
+  clgHands();
+});
+
+btnHit.addEventListener("click", function () {
+  // check if >= 21 points
+  if (points(playerHand) < 21) {
+    playerHand.push(deck.pop());
+    playerCardSlot.appendChild(playerHand[playerHand.length - 1].getHTML());
+    displayCards(playerCardSlot);
+    deckElement.textContent = deck.numberOfCards;
+
+    points(playerHand) < 21
+      ? (text.textContent = "Stand or you bust")
+      : points(playerHand) === 21
+      ? dealerFinish(true, "21 points!")
+      : dealerFinish(false, "You bust!");
+  }
+
+  clgHands();
+});
+
+// button active only when player has <21 points
+btnStand.addEventListener("click", function () {
+  dealerFinish(true, "You win/lose!!");
+
+  clgHands();
+
+  //payOut()
+});
